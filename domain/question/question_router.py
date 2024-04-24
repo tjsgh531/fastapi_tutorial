dbd0beb7023d9aa9e8d6e1a304c859f1d37732d8
sunho Lee <tjsgh5312@daum.net>
2024-04-24 17:29:05 +0900

    feat : Add Schema

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -4,12 +4,14 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import Question
 
+from domain.question import question_schema
+
 
 router = APIRouter(
     prefix="/question",
 )
 
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
 
     _question_list = db.query(Question).order_by(Question.createdate.desc()).all()
